c14a56fbb749c463787462e195cd8cff85cdbab1
longhoney <long.711.sg@gmail.com>
2026-06-11 21:16:28 +0700

    Khao sat xong nhanh dieu khien dong co

--- libraries/Makerlabvn_SimpleMotor_LONG/Makerlabvn_SimpleMotor_LONG.ino
+++ libraries/Makerlabvn_SimpleMotor_LONG/Makerlabvn_SimpleMotor_LONG.ino
@@ -1,116 +1,52 @@
-/*
-  Build for Arduino Uno/ MKE-Creator
-  Use 4 PWM pins // L298 6 Pins
-*/
-//Khai báo thư viện
+/**
+ * Kiểm tra điều khiển xe kết hợp 2 kênh A và B
+ * https://hshop.vn/mach-makeredu-creator-arduino-uno-compatible
+ */
+
+/* ------------------------------------------------------------------------- */
+/*                                  LIBRARY                                  */
+/* ------------------------------------------------------------------------- */
+
 #include "Makerlabvn_SimpleMotor_LONG.h"
 
-// Định nghĩa chân cho L9110
-// #define PIN_IN1 3  //! D3  (~) --> A-IB
-// #define PIN_IN2 9  //! D9  (~) --> A-IA (chan +)
-// #define PIN_IN3 10 //! D10 (~) --> B-IA (chan +)
-// #define PIN_IN4 11 //! D11 (~) --> B-IB
+/* ------------------------------------------------------------------------- */
+/*                                   DEFINE                                  */
+/* ------------------------------------------------------------------------- */
 
+// Creator
+#define PIN_ENA 5    //! (~)
+#define PIN_IN1 7
+#define PIN_IN2 4
+#define PIN_IN3 9
+#define PIN_IN4 8
+#define PIN_ENB 6    //! (~)
 
+/* ------------------------------------------------------------------------- */
+/*                                   OBJECT                                  */
+/* ------------------------------------------------------------------------- */
 
-// Định nghĩa chân cho MKE-Creator // KHONOG THAY DOI TOC DO DUOC
-// Định nghĩa chân cho L298
-#define PIN_ENA 6 //! D6 (~)
-#define PIN_IN1 8 
-#define PIN_IN2 9 
-#define PIN_IN3 4 
-#define PIN_IN4 7 
-#define PIN_ENB 5 //! D5 (~)
+Makerlabvn_SimpleMotor demo;
 
+/* ------------------------------------------------------------------------- */
+/*                                RUN ONE TIME                               */
+/* ------------------------------------------------------------------------- */
 
-//Khai báo đối tượng d9110 thuộc class Makerlabvn_SimpleMotor
-Makerlabvn_SimpleMotor_LONG d9110; //Not I2C 
-// Makerlabvn_SimpleMotor_LONG d9110(64); // For I2C
-
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
   Serial.begin(115200);
-  Serial.println("Start Test, input from 0 (OFF) to 8");
-  Serial.println("Baud 115200, No line ending");
-  // d9110.setup(PIN_IN1, PIN_IN2, PIN_IN3, PIN_IN4); //4pin
-  d9110.setup(PIN_ENA, PIN_IN1, PIN_IN2, PIN_IN3, PIN_IN4, PIN_ENB); //6pin
-  // d9110.setup(64); // For I2C
+  Serial.println("Start");
+
+  demo.setup(PIN_ENA, PIN_IN1, PIN_IN2, PIN_IN3, PIN_IN4, PIN_ENB);
+  Serial.println("Điều khiển xe...");
+  // Serial.println("Đi thẳng tốc độ 100%."); demo.car_fw(100, 100);
+  // Serial.println("Đi thẳng tốc độ 50%.");  demo.car_fw(50, 50);
+  Serial.println("Đi lùi tốc độ 100%.");  demo.car_bw(100, 100);
+  // Serial.println("Đi lùi tốc độ 50%."); demo.car_bw(50, 50);
 }
 
+/* ------------------------------------------------------------------------- */
+/*                                    MAIN                                   */
+/* ------------------------------------------------------------------------- */
+
 void loop() {
-  // put your main code here, to run repeatedly:
-  if (Serial.available() > 0) {
-    int tempInput = Serial.read();
-
-    switch (tempInput) {
-      case '0':
-        // digitalWrite(2, HIGH);
-        Serial.println("Hai động cơ đứng yên");
-        d9110.motorA_stop();
-        d9110.motorB_stop();
-        break;
-      case '1':
-        // digitalWrite(3, HIGH);
-        Serial.println("Điều khiển động cơ kênh A - Quay thuận 100%");
-        d9110.motorA_fw(100);
-        d9110.motorB_stop();
-        //delay(5000);
-        break;
-      case '2':
-        // digitalWrite(3, HIGH);
-        Serial.println("Điều khiển động cơ kênh A - Quay thuận 50%");
-        d9110.motorA_fw(50);
-        d9110.motorB_stop();
-        
-        // Serial.println("Xoay trái tốc độ 10%.");
-        // d9110.car_rotateL(30);
-        // delay(5000);
-        break;
-      case '3':
-        // digitalWrite(4, HIGH);
-        Serial.println("Điều khiển động cơ kênh A - Quay ngược 100%");
-        d9110.motorA_bw(100);
-        d9110.motorB_stop();
-        break;
-      case '4':
-        // digitalWrite(4, HIGH);
-        Serial.println("Điều khiển động cơ kênh A - Quay ngược 50%");
-        d9110.motorA_bw(50);
-        d9110.motorB_stop();
-        // Serial.println("Xoay phải tốc độ 50%.");
-        // d9110.car_rotateR(20);
-        // delay(5000);
-        break;
-      case '5':
-        // digitalWrite(5, HIGH);
-        Serial.println("Điều khiển động cơ kênh B - Quay thuận 100%");
-        d9110.motorB_fw(100);
-        d9110.motorA_stop();
-        break;
-      case '6':
-        // digitalWrite(5, HIGH);
-        Serial.println("Điều khiển động cơ kênh B - Quay thuận 50%");
-        d9110.motorB_fw(50);
-        d9110.motorA_stop();
-        break;
-      case '7':
-        // digitalWrite(6, HIGH);
-        Serial.println("Điều khiển động cơ kênh B - Quay ngược 100%");
-        d9110.motorB_bw(100);
-        d9110.motorA_stop();
-        break;
-      case '8':
-        // digitalWrite(6, HIGH);
-        Serial.println("Điều khiển động cơ kênh B - Quay ngược 50%");
-        d9110.motorB_bw(50);
-        d9110.motorA_stop();
-        break;
-      default:
-        // d9110.motorA_stop();
-        // d9110.motorB_stop();
-        break;
-        
-    }
-  }
-
 }
--- libraries/Makerlabvn_SimpleMotor_LONG/readme.md
+++ libraries/Makerlabvn_SimpleMotor_LONG/readme.md
@@ -1,24 +1,11 @@
-KHảo sát lại cách điều khiển động cơ (19/05/2026)
-================
-### Quy ước
+# Makerlabvn_SimpleMotor
 
-1. Thông số tốc độ: **0-100%** --> analogWrite() 0-255
-2. Thông số chiều quay thuận: **1** = Quay thuận chiều kim đồng hồ - xe đi tới. -->  digitalWrite(IN2,1); digitalWrite(IN1,0)
-3. Thông số chiều quay ngược: **0** = Quay ngược chiều kim đồng hồ - xe đi lùi. --> digitalWrite(IN2,0); digitalWrite(IN1,1)
+### Giới thiệu:
 
-<img width="974" height="469" alt="Image" src="https://github.com/user-attachments/assets/973f00b8-4a2d-416e-be78-ed209678afe1" />
-
-4. Dựa theo chú thích trên các mẫu driver phổ biến: Kênh A là motor bên trái, kênh B là motor bên phải
-5. Quy ước cho MKE-Creator
-<img width="1050" height="752" alt="Image" src="https://github.com/user-attachments/assets/32d34036-e4d2-4065-9a71-0eac56f71a4f" />
-
-5. Quy ước cho L298
-
-<img width="1056" height="484" alt="Image" src="https://github.com/user-attachments/assets/644791eb-c5ec-4065-8ea7-f3c9f8a78b27" />
-
-6. Quy ước cho L9110
-
-<img width="795" height="498" alt="Image" src="https://github.com/user-attachments/assets/48573e5f-2d96-4f1a-9e2b-1a0795e475a8" />
+> Đây là bộ thư viện điều khiển động cơ đơn giản trên Arduino giúp điều khiển **2 x Động cơ DC** trên các mạch Driver hỗ trợ điều khiển đơn giản bằng **4 chân tín hiệu** (2 chân tín hiệu trên mỗi động cơ), quy ước:
+>
+> - **MotorA:** `IN1/IN2` (chân `IN2` **<span style="color:#FE0101">bắt buộc** phải có chức năng xuất *xung PWM*).
+> - **MotorB:** `IN3/IN4` (chân `IN3` **<span style="color:#FE0101">bắt buộc** phải có chức năng xuất *xung PWM*).
 
 ## Hàm điều khiển motor
 
@@ -48,4 +35,14 @@ KHảo sát lại cách điều khiển động cơ (19/05/2026)
 |-|-|
 |**<span style="color:#E47128">car_stop()**|Điều khiển xe dừng lại.
 
-So sánh với thư viện: https://docs.google.com/spreadsheets/d/1pauv_eXI9WxPK2DNi59jDcf7WH-pYOTJTPTRPNfatLw/edit?usp=sharing
+> Để điều khiển xe đúng, cần kết nối Motor đến Driver đúng chiều.
+>
+> |Motor|Driver|
+> |-----|------|
+> |Motor Trái `(-)`|OUT1|
+> |Motor Trái `(+)`|OUT2|
+> |Motor Phải `(+)`|OUT3|
+> |Motor Phải `(-)`|OUT4|
+>
+> Trong đó, khi bạn cấp nguồn đúng cực `(+)` và `(-)` của Motor, sẽ làm motor quay hướng giúp xe đi tới.
+
